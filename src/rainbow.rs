adba037678b1f7318b71372bb1793887ddb62e02
David Knaack <davidkna@users.noreply.github.com>
2020-05-09 20:04:26 +0200

    colorize_str

--- src/rainbow.rs
+++ src/rainbow.rs
@@ -1,6 +1,7 @@
 use bstr::ByteSlice;
 use scarlet::{colors::cielchcolor::CIELCHColor, prelude::*};
 use std::io::Write;
+use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 pub struct Rainbow {
     current_row: i32,
@@ -48,6 +49,35 @@ impl Rainbow {
     pub fn colorize(&mut self, text: &[u8], out: &mut dyn Write) -> std::io::Result<()> {
         let mut escaping = false;
         for grapheme in text.graphemes() {
+            if grapheme == "\x1B" {
+                escaping = true;
+                continue;
+            }
+            if grapheme == "\n" {
+                self.reset_col();
+                self.step_row(1);
+                writeln!(out).unwrap();
+                continue;
+            }
+
+            if !escaping {
+                self.step_col(UnicodeWidthStr::width(grapheme) as i32);
+                let (r, g, b) = RGBColor::clamp(self.color)
+                    .convert::<RGBColor>()
+                    .int_rgb_tup();
+                write!(out, "\x1B[38;2;{};{};{}m{}", r, g, b, grapheme)?;
+            } else if "a" <= grapheme && "z" >= grapheme || "A" <= grapheme && "Z" >= grapheme {
+                escaping = false;
+            }
+        }
+
+        out.write_all(b"\x1B[0m")
+    }
+
+    #[allow(dead_code)]
+    pub fn colorize_str(&mut self, text: &str, out: &mut dyn Write) -> std::io::Result<()> {
+        let mut escaping = false;
+        for grapheme in UnicodeSegmentation::graphemes(text, true) {
             if grapheme == "\x1B" {
                 escaping = true;
                 continue;
