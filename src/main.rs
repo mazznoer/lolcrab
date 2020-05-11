b5f07f4c8f08f92544659e6a2e9d8111ddfe4f9b
David Knaack <davidkna@users.noreply.github.com>
2020-05-11 17:13:37 +0200

    optional mimalloc

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use std::{
 };
 use structopt::StructOpt;
 
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[derive(StructOpt)]
 #[structopt(name = "lcat", about = "Terminal rainbows.")]
 pub struct Cmdline {
