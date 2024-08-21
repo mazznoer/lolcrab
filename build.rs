a912c4f6b1a27598f52e919ae7578b3059604ae1
Nor Khasyatillah <mazznoer@ymail.com>
2024-08-21 18:11:05 +0700

    update build.rs

--- build.rs
+++ build.rs
@@ -1,18 +1,15 @@
 #[cfg(feature = "cli")]
-use clap::CommandFactory;
-#[cfg(feature = "cli")]
-use clap_complete::{generate_to, Shell};
-#[cfg(feature = "cli")]
-use std::env;
-
-#[cfg(feature = "cli")]
 include!("src/cli.rs");
 
 #[cfg(feature = "cli")]
 fn main() -> Result<(), clap::Error> {
-    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions/");
+    use clap::CommandFactory;
+    use clap_complete::{generate_to, Shell};
+    use std::{env, fs, path};
+
+    let outdir = path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions/");
     if !outdir.exists() {
-        std::fs::create_dir(outdir.clone()).expect("Failed to create 'completions' directory.");
+        fs::create_dir(outdir.clone()).expect("Failed to create 'completions' directory.");
     }
 
     let mut cmd = Opt::command();
